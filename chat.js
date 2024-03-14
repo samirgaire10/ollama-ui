5073adbca99ecd3cbc9f6cdea232854456651d57
samirgaire10 <samirgaire100@protonmail.com>
2024-03-15 03:32:43 +0900

    ollama ui changes

--- chat.js
+++ chat.js
@@ -301,3 +301,11 @@ function updateChatList() {
   }
 }
 
+
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
