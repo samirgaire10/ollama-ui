5f991d49df2f1fb5905bb561fba766cd09de7f78
samirgaire10 <samirgaire100@protonmail.com>
2024-03-18 21:48:27 +0900

    hide and show

--- chat.js
+++ chat.js
@@ -331,9 +331,11 @@ function updateChatList() {
 function openNav() {
   document.getElementById("mySidebar").style.width = "250px";
   document.getElementById("main").style.marginLeft = "250px";
+  document.getElementById("hide").style.display = "none";
 }
 
 function closeNav() {
   document.getElementById("mySidebar").style.width = "0";
   document.getElementById("main").style.marginLeft = "0";
+  document.getElementById("hide").style.display = "block";
 }
